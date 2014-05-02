9a0b574bc9c34702bc770829126084829aa0c2f5
James Marshall <jcmarsh@gwmail.gwu.edu>
2014-05-02 17:35:22 -0400

    Converting benchmarker into a player translator and a benchmarker.

--- player_drivers/translator/translator_driver.cc
+++ player_drivers/translator/translator_driver.cc
@@ -1,6 +1,7 @@
 /*
- * Interposes upon the base player setup and whatever I am trying to 
- * benchmark. For now it is specifically for art_pot / voter_b.
+ * Translates (and isolates) Player to pipes.
+ * 
+ * James Marshall
  */
 
 #include <math.h> // TODO: REVIEW
@@ -20,10 +21,10 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // The class for the driver
-class BenchmarkerDriver : public ThreadedDriver {
+class TranslatorDriver : public ThreadedDriver {
 public:
   // Constructor; need that
-  BenchmarkerDriver(ConfigFile* cf, int section);
+  TranslatorDriver(ConfigFile* cf, int section);
 
   // This method will be invoked on each incoming message
   virtual int ProcessMessage(QueuePointer &resp_queue, 
@@ -54,7 +55,7 @@ private:
 
   void SendWaypoints();
 
-  // Devices provided - These are how to send goals to the benchmarker.
+  // Devices provided - These are how to send goals to the translator.
   player_devaddr_t cmd_out_odom; // "original:localhost:6666:position2d:1"
 
   // Required devices (odometry and ranger)
@@ -85,25 +86,25 @@ private:
 // declare it static in the class).  In this function, we create and return
 // (as a generic Driver*) a pointer to a new instance of this driver.
 Driver* 
-BenchmarkerDriver_Init(ConfigFile* cf, int section)
+TranslatorDriver_Init(ConfigFile* cf, int section)
 {
   // Create and return a new instance of this driver
-  return((Driver*)(new BenchmarkerDriver(cf, section)));
+  return((Driver*)(new TranslatorDriver(cf, section)));
 }
 
 // A driver registration function, again declared outside of the class so
 // that it can be invoked without object context.  In this function, we add
 // the driver into the given driver table, indicating which interface the
 // driver can support and how to create a driver instance.
-void BenchmarkerDriver_Register(DriverTable* table)
+void TranslatorDriver_Register(DriverTable* table)
 {
-  table->AddDriver("benchmarkerdriver", BenchmarkerDriver_Init);
+  table->AddDriver("translatordriver", TranslatorDriver_Init);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Constructor.  Retrieve options from the configuration file and do any
 // pre-Setup() setup.
-BenchmarkerDriver::BenchmarkerDriver(ConfigFile* cf, int section)
+TranslatorDriver::TranslatorDriver(ConfigFile* cf, int section)
   : ThreadedDriver(cf, section)
 {
   // Check for position2d for commands
@@ -141,11 +142,11 @@ BenchmarkerDriver::BenchmarkerDriver(ConfigFile* cf, int section)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set up the device.  Return 0 if things go well, and -1 otherwise.
-int BenchmarkerDriver::MainSetup()
+int TranslatorDriver::MainSetup()
 {   
   int index = 0;
 
-  puts("Benchmarker driver initialising in MainSetup");
+  puts("Translator driver initialising in MainSetup");
 
   // TODO: Is this moving player to cpu 3? Need to review
   InitTAS(3, &cpu_speed);
@@ -169,16 +170,16 @@ int BenchmarkerDriver::MainSetup()
   forkSingleReplica(&repGroup, 0, "VoterB");
   //  printf("Replica - pid: %d\tfd_in_r: %d\tfd_in_w: %d\n", repGroup.replicas[0].pid, repGroup.replicas[0].pipefd_into_rep[0], repGroup.replicas[0].pipefd_into_rep[1]);
 
-  puts("Benchmarker driver ready");
+  puts("Translator driver ready");
 
   return(0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shutdown the device
-int BenchmarkerDriver::MainShutdown()
+int TranslatorDriver::MainShutdown()
 {
-  puts("Shutting Benchmarker driver down");
+  puts("Shutting Translator driver down");
 
   if(this->ranger) {
     this->ShutdownRanger();
@@ -186,13 +187,13 @@ int BenchmarkerDriver::MainShutdown()
 
   ShutdownOdom();
 
-  puts("Benchmarker driver has been shutdown");
+  puts("Translator driver has been shutdown");
   return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Incoming message!
-int BenchmarkerDriver::ProcessMessage(QueuePointer & resp_queue, 
+int TranslatorDriver::ProcessMessage(QueuePointer & resp_queue, 
                                   player_msghdr * hdr,
                                   void * data)
 {
@@ -220,13 +221,13 @@ int BenchmarkerDriver::ProcessMessage(QueuePointer & resp_queue,
     assert(hdr->size == sizeof(player_position2d_cmd_pos_t));
     ProcessCommand(*reinterpret_cast<player_position2d_cmd_pos_t *> (data));
   } else {
-    puts("Benchmarker: I don't know what to do with that.");
+    puts("Translator: I don't know what to do with that.");
     // Message not dealt with with
     return -1;
   }
 }
 
-void BenchmarkerDriver::SendWaypoints() {
+void TranslatorDriver::SendWaypoints() {
   struct comm_header hdr;
   struct comm_way_res_msg message;
 
@@ -240,7 +241,7 @@ void BenchmarkerDriver::SendWaypoints() {
   write(replicas[0].pipefd_into_rep[1], (void*)(&message), sizeof(struct comm_header) + hdr.byte_count);
 }
 
-void BenchmarkerDriver::Main() {
+void TranslatorDriver::Main() {
   for(;;) {
     //    Wait(0.0001);
     this->DoOneUpdate();
@@ -249,7 +250,7 @@ void BenchmarkerDriver::Main() {
 }
 
 // Called by player for each non-threaded driver.
-void BenchmarkerDriver::DoOneUpdate() {
+void TranslatorDriver::DoOneUpdate() {
   int retval;
   struct comm_header hdr;
   double cmd_vel[2];
@@ -269,7 +270,7 @@ void BenchmarkerDriver::DoOneUpdate() {
       this->PutCommand(cmd_vel[0], cmd_vel[1]);
       break;
     default:
-      printf("ERROR: Benchmarker can't handle comm type: %d\n", hdr.type);
+      printf("ERROR: Translator can't handle comm type: %d\n", hdr.type);
     }
   }
 
@@ -288,16 +289,16 @@ void BenchmarkerDriver::DoOneUpdate() {
 extern "C" {
   int player_driver_init(DriverTable* table)
   {
-    puts("Benchmarker driver initializing");
-    BenchmarkerDriver_Register(table);
-    puts("Benchmarker driver done");
+    puts("Translator driver initializing");
+    TranslatorDriver_Register(table);
+    puts("Translator driver done");
     return(0);
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shutdown the underlying odom device.
-int BenchmarkerDriver::ShutdownOdom()
+int TranslatorDriver::ShutdownOdom()
 {
   // Stop the robot before unsubscribing
   this->PutCommand(0, 0);
@@ -308,7 +309,7 @@ int BenchmarkerDriver::ShutdownOdom()
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shut down the ranger
-int BenchmarkerDriver::ShutdownRanger()
+int TranslatorDriver::ShutdownRanger()
 {
   this->ranger->Unsubscribe(this->InQueue);
   return 0;
@@ -316,7 +317,7 @@ int BenchmarkerDriver::ShutdownRanger()
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set up the underlying odom device.
-int BenchmarkerDriver::SetupOdom()
+int TranslatorDriver::SetupOdom()
 {
   if(!(this->odom = deviceTable->GetDevice(this->odom_addr)))
   {
@@ -334,7 +335,7 @@ int BenchmarkerDriver::SetupOdom()
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set up the ranger
-int BenchmarkerDriver::SetupRanger()
+int TranslatorDriver::SetupRanger()
 {
   if(!(this->ranger = deviceTable->GetDevice(this->ranger_addr))) {
     PLAYER_ERROR("unable to locate suitable ranger device");
@@ -350,7 +351,7 @@ int BenchmarkerDriver::SetupRanger()
 
 ////////////////////////////////////////////////////////////////////////////////
 // Process new odometry data
-void BenchmarkerDriver::ProcessOdom(player_position2d_data_t &data)
+void TranslatorDriver::ProcessOdom(player_position2d_data_t &data)
 {
   struct comm_header hdr;
   struct comm_pos_data_msg message;
@@ -368,7 +369,7 @@ void BenchmarkerDriver::ProcessOdom(player_position2d_data_t &data)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Process ranger data
-void BenchmarkerDriver::ProcessRanger(player_ranger_data_range_t &data)
+void TranslatorDriver::ProcessRanger(player_ranger_data_range_t &data)
 {
   int index = 0;
   struct comm_header hdr;
@@ -392,7 +393,7 @@ void BenchmarkerDriver::ProcessRanger(player_ranger_data_range_t &data)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Send commands to underlying position device
-void BenchmarkerDriver::PutCommand(double cmd_speed, double cmd_turnrate)
+void TranslatorDriver::PutCommand(double cmd_speed, double cmd_turnrate)
 {
   player_position2d_cmd_vel_t cmd;
 #ifdef _STATS_BENCH_ROUND_TRIP_
@@ -416,7 +417,7 @@ void BenchmarkerDriver::PutCommand(double cmd_speed, double cmd_turnrate)
 		     (void*)&cmd, sizeof(cmd), NULL);
 }
 
-void BenchmarkerDriver::ProcessCommand(player_position2d_cmd_pos_t &cmd) {
+void TranslatorDriver::ProcessCommand(player_position2d_cmd_pos_t &cmd) {
   curr_goal[INDEX_X] = cmd.pos.px;
   curr_goal[INDEX_Y] = cmd.pos.py;
   curr_goal[INDEX_A] = cmd.pos.pa;
