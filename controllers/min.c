8a712fe7fe67ed17762f8d844de79c37c8c8d028
James <jcmarsh@gwmail.gwu.edu>
2015-01-18 15:07:37 -0500

    Added a signal handler to Min to let it use the module.

--- controllers/min.c
+++ controllers/min.c
@@ -10,7 +10,21 @@
 #include "../include/replicas.h"
 #include "../include/commtypes.h"
 
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#define __NR_inspect 312
+
 #define REP_COUNT 2
+
+long inspect_syscall(void) {
+  return syscall(__NR_inspect);
+}
+
+void syscallHandler(int signo) {
+  long int retval = inspect_syscall();
+  printf("Hopefully called the syscall. retval = %ld\n", retval);
+  return;
+}
 
 // Replica related data
 struct replica replicas[REP_COUNT];
@@ -21,6 +35,11 @@ int pipe_count = 0;
 struct typed_pipe ext_pipes[PIPE_LIMIT];
 
 int main(int argc, const char **argv) {
+  if (signal(SIGUSR2, syscallHandler) == SIG_ERR) {
+    perror("Failed to register the invoke syscall handler");
+    return -1;
+  }
+
   controller_name = const_cast<char*>(argv[1]);
 
   initReplicas(replicas, REP_COUNT, controller_name, 10);
