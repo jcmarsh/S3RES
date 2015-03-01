749930241a6f94cdc58ecc23fefe2fe167abc711
James <jcmarsh@gwmail.gwu.edu>
2015-03-01 21:47:31 +0000

    Added art_pot test which should demonstrate issue in VoterM. Reopen #55.

---
+++ test/art_pot_vote_test.c
@@ -0,0 +1,110 @@
+// Test filter
+
+#include "test.h"
+
+const char* controller_name = "ArtPot";
+
+int main(int argc, const char** argv) {
+  // VoterD ArtPot TMR 400 70 RANGE_POSE_DATA:18:0:1 WAY_REQ:0:31:0 WAY_RES:32:0:0 MOV_CMD:0:16:1
+
+  pid_t currentPID = 0;
+  char** rep_argv;
+
+  int ranger_in[2], way_out[2], way_in[2], move_out[2];
+
+  pipe(ranger_in);
+  pipe(way_out);
+  pipe(way_in);
+  pipe(move_out);
+
+  rep_argv = malloc(sizeof(char *) * 10);
+  rep_argv[0] = "VoterM";
+  //  rep_argv[0] = "VoterD";
+  rep_argv[1] = "ArtPot";
+  rep_argv[2] = "TMR";
+  rep_argv[3] = "800"; // Timeout
+  rep_argv[4] = "80";  // priority
+  //  rep_argv[3] = "1800";
+  //  rep_argv[4] = "1";
+  asprintf(&rep_argv[5], "%s:%d:%d:%d", "RANGE_POSE_DATA", ranger_in[0], 0, 1);
+  asprintf(&rep_argv[6], "%s:%d:%d:%d", "WAY_REQ", 0, way_out[1], 0);
+  asprintf(&rep_argv[7], "%s:%d:%d:%d", "WAY_RES", way_in[0], 0, 0);
+  asprintf(&rep_argv[8], "%s:%d:%d:%d", "MOV_CMD", 0, move_out[1], 1);
+  rep_argv[9] = NULL;
+
+  // Need to fork exec VoterM, but first need a pipe in and two outs.
+  currentPID = fork();
+
+  if (currentPID >= 0) { // Successful fork
+    if (currentPID == 0) { // Child process
+      if (-1 == execv(rep_argv[0], rep_argv)) {
+	printf("Exec ERROR!\n");
+      }
+    }
+  }
+
+  sleep(2);
+
+  printf("Waiting for Voter to forward waypoint request.\n");
+  // First need to read the waypoint request
+  struct comm_way_req way_req;
+  read(way_out[0], &way_req, sizeof(way_req));
+  printf("Got waypoint req\n");
+
+  // Send response
+  struct comm_way_res way_res;
+  way_res.point[0] = 8.0;
+  way_res.point[1] = 8.0;
+  way_res.point[2] = 0.0;
+  write(way_in[1], &way_res, sizeof(way_res));
+
+  int loops = 100;
+  while (loops--) {
+    // Create and send some ranger data
+    struct comm_range_pose_data sim_range_data;
+    sim_range_data.pose[0] = -2.0;
+    sim_range_data.pose[1] = -2.0;
+    sim_range_data.pose[2] = 1.0;
+    int i = 0;
+    for (i = 0; i < 16; i++) {
+      sim_range_data.ranges[i] = i * 1.5;
+    }
+    printf("Writing data to pipes\n");
+    write(ranger_in[1], &sim_range_data, sizeof(struct comm_range_pose_data));
+
+    // check for command out or a new waypoint request
+    int retval = 0;
+
+    struct timeval select_timeout;
+    fd_set select_set;
+
+    // See if any of the read pipes have anything
+    select_timeout.tv_sec = 1;
+    select_timeout.tv_usec = 0;
+
+    FD_ZERO(&select_set);
+    FD_SET(way_out[0], &select_set);
+    FD_SET(move_out[0], &select_set);
+
+    printf("The select.\n");
+    retval = select(FD_SETSIZE, &select_set, NULL, NULL, &select_timeout);
+
+    if (retval > 0) {
+      if (FD_ISSET(way_out[0], &select_set)) {
+        read(way_out[0], &way_req, sizeof(way_req));
+        printf("Got waypoint req\n");
+        way_res.point[0] = 8.0;
+        way_res.point[1] = 8.0;
+        way_res.point[2] = 0.0;
+        write(rep.vot_pipes[2].fd_out, &way_res, sizeof(way_res));
+      }
+      if (FD_ISSET(move_out[0], &select_set)) {
+        // read the command out
+        struct comm_mov_cmd mov_cmd;
+        read(move_out[0], &mov_cmd, sizeof(mov_cmd));
+        printf("Move Command: %f, %f\n", mov_cmd.vel_cmd[0], mov_cmd.vel_cmd[1]);
+      }
+    }
+    sleep(1);
+  }
+}
--- test/filter_vote_test.c
+++ test/filter_vote_test.c
@@ -16,10 +16,9 @@ int main(int argc, const char** argv) {
   pipe(pipe_out1);
   pipe(pipe_out2);
 
-
   rep_argv = malloc(sizeof(char *) * 10);
-  rep_argv[0] = "VoterM";
-  //  rep_argv[0] = "VoterD";
+  //  rep_argv[0] = "VoterM";
+  rep_argv[0] = "VoterD";
   rep_argv[1] = "Filter";
   rep_argv[2] = "TMR";
   rep_argv[3] = "800"; // Timeout
@@ -28,8 +27,8 @@ int main(int argc, const char** argv) {
   //  rep_argv[4] = "1";
   asprintf(&rep_argv[5], "%s:%d:%d:%d", "RANGE_POSE_DATA", pipe_in[0], 0, 1);
   asprintf(&rep_argv[6], "%s:%d:%d:%d", "RANGE_POSE_DATA", 0, pipe_out0[1], 1);
-  asprintf(&rep_argv[7], "%s:%d:%d:%d", "RANGE_POSE_DATA", 0, pipe_out1[1], 1);
-  asprintf(&rep_argv[8], "%s:%d:%d:%d", "RANGE_POSE_DATA", 0, pipe_out2[1], 1);
+  asprintf(&rep_argv[7], "%s:%d:%d:%d", "RANGE_POSE_DATA", 0, pipe_out1[1], 0);
+  asprintf(&rep_argv[8], "%s:%d:%d:%d", "RANGE_POSE_DATA", 0, pipe_out2[1], 0);
   rep_argv[9] = NULL;
 
   // Need to fork exec VoterM, but first need a pipe in and two outs.
@@ -43,28 +42,33 @@ int main(int argc, const char** argv) {
     }
   }
 
+  sleep(2);
+
   // Should be connected now.
   double pose_add = 0.0;
   int loops = 10000000;
   while (loops--) {
-	// Create and send some ranger data
-  	struct comm_range_pose_data sim_range_data;
-  	sim_range_data.pose[0] = -2.0 + pose_add++;
-  	sim_range_data.pose[1] = -5.0;
-  	sim_range_data.pose[2] = 1.0;
-  	int i = 0;
-  	for (i = 0; i < 16; i++) {
-  		sim_range_data.ranges[i] = i * 1.5;
-  	}
+    // Create and send some ranger data
+    struct comm_range_pose_data sim_range_data;
+    sim_range_data.pose[0] = -2.0 + pose_add++;
+    sim_range_data.pose[1] = -5.0;
+    sim_range_data.pose[2] = 1.0;
+    int i = 0;
+    for (i = 0; i < 16; i++) {
+      sim_range_data.ranges[i] = i * 1.5;
+    }
 
-  	write(pipe_in[1], &sim_range_data, sizeof(struct comm_range_pose_data));
+    printf("Writing the datas.\n");
+    write(pipe_in[1], &sim_range_data, sizeof(struct comm_range_pose_data));
 
     // read filtered data
     read(pipe_out0[0], &sim_range_data, sizeof(sim_range_data));
+    printf("Does order here matter?\n");
     read(pipe_out1[0], &sim_range_data, sizeof(sim_range_data));
     read(pipe_out2[0], &sim_range_data, sizeof(sim_range_data));
     printf("Pose returned %f, %f\n", sim_range_data.pose[0], sim_range_data.pose[1]);
 
-    usleep(1000000);
+    //usleep(1000000);
+    sleep(1);
   }
 }
