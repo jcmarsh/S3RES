1b874dad74632871a1833a2005ddf1c9462b288b
James <jcmarsh@gwmail.gwu.edu>
2015-08-02 18:50:13 -0500

    Closes #35. Micro_test now builds. Still improvements to be made.

--- test/micro_test/empty_bench.c
+++ test/micro_test/empty_bench.c
@@ -1,15 +1,14 @@
-// Test filter
+// Micro benchmark for round trip communication with an Empty controller
 
 #include <time.h>
 
-#include "../include/taslimited.h"
-#include "../include/commtypes.h"
-#include "../include/replicas.h"
-#include "../include/fd_server.h"
+#include "taslimited.h"
+#include "commtypes.h"
+#include "replicas.h"
 
 struct replica rep;
 const char* controller_name = "Empty";
-struct typed_pipe pipes[PIPE_LIMIT];
+struct vote_pipe pipes[PIPE_LIMIT];
 
 // FD server
 struct server_data sd;
@@ -23,11 +22,11 @@ int main(int argc, const char** argv) {
 
   // Setup pipe type and direction
   // Ranger data in
-  pipes[0].type = RANGE_POSE_DATA;
+  pipes[0].rep_info = (char *) MESSAGE_T[RANGE_POSE_DATA];
   pipes[0].fd_in = 42;
   pipes[0].fd_out = 0;
   // Move Command Out
-  pipes[1].type = MOV_CMD;
+  pipes[1].rep_info = (char *) MESSAGE_T[MOV_CMD];
   pipes[1].fd_in = 0;
   pipes[1].fd_out = 42;
 
--- test/micro_test/empty_restart.c
+++ test/micro_test/empty_restart.c
@@ -1,10 +1,10 @@
-// Test Empty
+// Test restart times for the Empty controller
 
 #include <time.h>
 
-#include "../include/controller.h"
-#include "../include/replicas.h"
-#include "../include/fd_server.h"
+#include "taslimited.h"
+#include "controller.h"
+#include "replicas.h"
 
 // Replica related data
 struct replica replicas[2];
@@ -20,21 +20,32 @@ const char* controller_name = "Empty";
 
 // pipes to external components (not replicas)
 int pipe_count = 2;
-struct typed_pipe ext_pipes[2];
+struct vote_pipe ext_pipes[2];
 
 int main(int argc, const char **argv) {
   InitTAS(DEFAULT_CPU, voter_priority);
 
   // Setup fd server
   createFDS(&sd, controller_name);
-  startReplicas();
+
+  // Setup pipe type and direction
+  // Ranger data in
+  ext_pipes[0].rep_info = (char *) MESSAGE_T[RANGE_POSE_DATA];
+  ext_pipes[0].fd_in = 42;
+  ext_pipes[0].fd_out = 0;
+  // Move Command Out
+  ext_pipes[1].rep_info = (char *) MESSAGE_T[MOV_CMD];
+  ext_pipes[1].fd_in = 0;
+  ext_pipes[1].fd_out = 42;
+
+  startReplicas(replicas, rep_count, &sd, controller_name, ext_pipes, 2, voter_priority - 5);
 
   int loops = 1000;
   while(loops--) {
     // pick one at random
     int restartee = rand() % 2;
 
-    //int restarter = (restartee + (rep_count - 1)) % rep_count;
+    int restarter = (restartee + (rep_count - 1)) % rep_count;
     //timestamp_t curr_time = generate_timestamp();
     //union sigval time_value;
     //time_value.sival_ptr = (void *)curr_time;
@@ -45,21 +56,16 @@ int main(int argc, const char **argv) {
 
     // restart it
     timestamp_t last = generate_timestamp();
-    restartReplica(restartee);
+    restartReplica(replicas, rep_count, &sd, ext_pipes, restarter, restartee, voter_priority - 5);
     timestamp_t current = generate_timestamp();
     printf("(%lld)\n", current - last);
     
     usleep(100000);
   }
 
-  while(1) {
-    // restart it
-    timestamp_t last = generate_timestamp();
-    //restartReplica(restartee);
-    timestamp_t current = generate_timestamp();
-    printf("(%lld)\n", current - last);
-    
-    usleep(100000);
+  int i;
+  for (i = 0; i < rep_count; i++) {
+    kill(replicas[i].pid, SIGKILL);
   }
 
   return 0;
