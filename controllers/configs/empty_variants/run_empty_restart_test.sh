b926733c4a096806e9525d9fbcde06edf7861cf8
James Marshall <jcmarsh@gwmail.gwu.edu>
2015-10-02 10:48:04 -0400

    Closes #42. Can't believe I didn't use a function for this earlier.

--- controllers/configs/empty_variants/run_empty_restart_test.sh
+++ controllers/configs/empty_variants/run_empty_restart_test.sh
@@ -4,242 +4,72 @@ PLAYER_TIME=920s
 BASIC_TIME=910s
 ANOTHER_TIME=900s
 
-SIM_IP=192.168.100.1
+SIM_IP=161.253.66.53
+
+ITARS=1
 
 PINT_DIR=/home/jcmarsh/research/PINT
 CONFIG_DIR=$PINT_DIR/controllers/configs
 
 # Check bench_config, do you want to record response times or restart times?
 
+# Two args: $1 is the file name prefix, $2 is the postfix... kinda.
+runExperiment () {
+	cp $CONFIG_DIR/empty_variants/Empty_$2 ./Empty
+	for index in `seq 0 $ITARS`; do
+		timeout $PLAYER_TIME player baseline.cfg > $1_empty_restart_$2_$index.txt &
+		sleep 5
+		timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
+		sleep 5
+		ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > $1_empty_restart_injector_$2_$index.txt
+		timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> $1_empty_restart_injector_$2_$index.txt &
+		sleep $ANOTHER_TIME
+		ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> $1_empty_restart_injector_$2_$index.txt
+		sleep 60
+	done	
+}
+
 ###### SMR CASE ##########################################################
 cp $CONFIG_DIR/smr_empty.cfg ./config_plumber.cfg
-# Empty Tri restart kill -9 Empty_01092
-cp $CONFIG_DIR/empty_variants/Empty_01092 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > smr_empty_restart_01092_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > smr_empty_restart_injector_01092_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> smr_empty_restart_injector_01092_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> smr_empty_restart_injector_01092_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_02048
-cp $CONFIG_DIR/empty_variants/Empty_02048 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > smr_empty_restart_02048_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > smr_empty_restart_injector_02048_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> smr_empty_restart_injector_02048_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> smr_empty_restart_injector_02048_$index.txt
-	sleep 60
-done
+runExperiment smr 01092
+runExperiment smr 02048
+runExperiment smr 03072
+runExperiment smr 04096
+runExperiment smr 06144
+runExperiment smr 08192
+runExperiment smr 12288
+runExperiment smr 16384
+runExperiment smr 24576
+runExperiment smr 32768
+runExperiment smr 49152
+runExperiment smr 65536
 
 ###### DMR CASE ##########################################################
 cp $CONFIG_DIR/dmr_empty.cfg ./config_plumber.cfg
-# Empty DMR restart kill -9 Empty_01092
-cp $CONFIG_DIR/empty_variants/Empty_01092 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > dmr_empty_restart_01092_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > dmr_empty_restart_injector_01092_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> dmr_empty_restart_injector_01092_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> dmr_empty_restart_injector_01092_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_02048
-cp $CONFIG_DIR/empty_variants/Empty_02048 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > dmr_empty_restart_02048_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > dmr_empty_restart_injector_02048_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> dmr_empty_restart_injector_02048_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> dmr_empty_restart_injector_02048_$index.txt
-	sleep 60
-done
-
-exit
+runExperiment dmr 01092
+runExperiment dmr 02048
+runExperiment dmr 03072
+runExperiment dmr 04096
+runExperiment dmr 06144
+runExperiment dmr 08192
+runExperiment dmr 12288
+runExperiment dmr 16384
+runExperiment dmr 24576
+runExperiment dmr 32768
+runExperiment dmr 49152
+runExperiment dmr 65536
 
 ###### TMR CASE ##########################################################
 cp $CONFIG_DIR/tmr_empty.cfg ./config_plumber.cfg
-# Empty Tri restart kill -9 Empty_01092
-cp $CONFIG_DIR/empty_variants/Empty_01092 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_01092_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_01092_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_01092_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_01092_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_02048
-cp $CONFIG_DIR/empty_variants/Empty_02048 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_02048_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_02048_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_02048_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_02048_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_03072
-cp $CONFIG_DIR/empty_variants/Empty_03072 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_03072_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_03072_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_03072_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_03072_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_04096
-cp $CONFIG_DIR/empty_variants/Empty_04096 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_04096_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_04096_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_04096_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_04096_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_06144
-cp $CONFIG_DIR/empty_variants/Empty_06144 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_06144_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_06144_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_06144_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_06144_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_08192
-cp $CONFIG_DIR/empty_variants/Empty_08192 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_08192_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_08192_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_08192_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_08192_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_12288
-cp $CONFIG_DIR/empty_variants/Empty_12288 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_12288_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_12288_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_12288_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_12288_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_16384
-cp $CONFIG_DIR/empty_variants/Empty_16384 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_16384_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_16384_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_16384_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_16384_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_24576
-cp $CONFIG_DIR/empty_variants/Empty_24576 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_24576_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_24576_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_24576_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_24576_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_32768
-cp $CONFIG_DIR/empty_variants/Empty_32768 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_32768_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_32768_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_32768_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_32768_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_49152
-cp $CONFIG_DIR/empty_variants/Empty_49152 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_49152_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_49152_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_49152_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_49152_$index.txt
-	sleep 60
-done
-
-# Empty Tri restart kill -9 Empty_65536
-cp $CONFIG_DIR/empty_variants/Empty_65536 ./Empty
-for index in `seq 0 1`; do
-	timeout $PLAYER_TIME player baseline.cfg > empty_restart_test_Empty_65536_$index.txt &
-	sleep 5
-	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
-	sleep 5
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > empty_restart_test_injector_Empty_65536_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" "Empty" >> empty_restart_test_injector_Empty_65536_$index.txt &
-	sleep $ANOTHER_TIME
-	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> empty_restart_test_injector_Empty_65536_$index.txt
-	sleep 60
-done
-
+runExperiment tmr 01092
+runExperiment tmr 02048
+runExperiment tmr 03072
+runExperiment tmr 04096
+runExperiment tmr 06144
+runExperiment tmr 08192
+runExperiment tmr 12288
+runExperiment tmr 16384
+runExperiment tmr 24576
+runExperiment tmr 32768
+runExperiment tmr 49152
+runExperiment tmr 65536
