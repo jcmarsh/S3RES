15c532f4a1bb3eac5c19e975af12192b663470d6
James <jcmarsh@gwmail.gwu.edu>
2016-03-19 23:36:38 -0400

    Updated configurations and mini test script.

--- controllers/configs/run_mini_test.sh
+++ controllers/configs/run_mini_test.sh
@@ -8,13 +8,13 @@ SIM_IP=192.168.0.101
 
 PINT_DIR=/home/jcmarsh/research/PINT
 CONFIG_DIR=$PINT_DIR/controllers/configs
-OUTPUT=/home/jcmarsh/Dropbox/research/MyPaperAttempts/RTAS16/mini_test/latest_run
+OUTPUT=/home/jcmarsh/Dropbox/research/EMSOFT16/mini_test/latest_run
 
-ITARS=99
+ITARS=9
 
 # Now all with load
 
-# All baseline
+# 00 All baseline
 cp $CONFIG_DIR/all_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > baseline_all_wl_$index.txt &
@@ -28,7 +28,7 @@ mv *.txt $OUTPUT/all_wl/
 
 sleep 60
 
-# All Tri baseline
+# 01 All Tri baseline
 cp $CONFIG_DIR/all_tri_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > baseline_all_tri_wl_$index.txt &
@@ -42,7 +42,7 @@ mv *.txt $OUTPUT/all_tri_wl/
 
 sleep 60
 
-# Reactive Tri, other DMR baseline
+# 02 Reactive Tri, other DMR baseline
 cp $CONFIG_DIR/rl_tri_other_dmr_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > baseline_rl_tri_other_dmr_wl_$index.txt &
@@ -56,7 +56,7 @@ mv *.txt $OUTPUT/rl_other_wl/
 
 sleep 60
 
-# Artpot Tri, planning and mapper DMR baseline, Filter SMR
+# 03 Artpot Tri, planning and mapper DMR baseline, Filter SMR
 cp $CONFIG_DIR/art_tmr_planning_dmr_filter_smr_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > baseline_art_tmr_planning_dmr_filter_smr_wl_$index.txt &
@@ -70,7 +70,7 @@ mv *.txt $OUTPUT/art_wl/
 
 sleep 60
 
-# Artpot Tri, planning and mapper DMR baseline, Filter SMR
+# 04 Artpot Tri, planning and mapper DMR baseline, Filter SMR
 cp $CONFIG_DIR/filter_smr_all_tmr_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > baseline_filter_smr_wl_$index.txt &
@@ -84,7 +84,7 @@ mv *.txt $OUTPUT/filter_wl/
 
 sleep 60
 
-# Art kill -9 tests
+# 05 Art kill -9 tests
 cp $CONFIG_DIR/art_tmr_planning_dmr_filter_smr_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > art_kill_test_tri_wl_$index.txt &
@@ -92,7 +92,7 @@ for index in `seq 0 $ITARS`; do
 	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
 	sleep 5
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > art_kill_test_tri_injector_wl_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" >> art_kill_test_tri_injector_wl_$index.txt &
+	timeout $ANOTHER_TIME python injector.py false "kill -9" >> art_kill_test_tri_injector_wl_$index.txt &
 	sleep $ANOTHER_TIME
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> art_kill_test_tri_injector_wl_$index.txt
 	sleep 40
@@ -102,7 +102,7 @@ mv *.txt $OUTPUT/art_kill_wl/
 
 sleep 60
 
-# All Tri kill -9 tests
+# 06 All Tri kill -9 tests
 cp $CONFIG_DIR/all_tri_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > kill_test_tri_wl_$index.txt &
@@ -110,7 +110,7 @@ for index in `seq 0 $ITARS`; do
 	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
 	sleep 5
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > kill_test_tri_injector_wl_$index.txt
-	timeout $ANOTHER_TIME python injector.py "kill -9" >> kill_test_tri_injector_wl_$index.txt &
+	timeout $ANOTHER_TIME python injector.py false "kill -9" >> kill_test_tri_injector_wl_$index.txt &
 	sleep $ANOTHER_TIME
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> kill_test_tri_injector_wl_$index.txt
 	sleep 40
@@ -120,7 +120,7 @@ mv *.txt $OUTPUT/kill_wl/
 
 sleep 60
 
-# All Tri /bin/kill -s 37 tests (simulated sdc)
+# 07 All Tri /bin/kill -s 37 tests (simulated sdc)
 cp $CONFIG_DIR/all_tri_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > sdc_test_tri_wl_$index.txt &
@@ -128,7 +128,7 @@ for index in `seq 0 $ITARS`; do
 	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
 	sleep 5
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > sdc_test_tri_injector_wl_$index.txt
-	timeout $ANOTHER_TIME python injector.py "/bin/kill -s 37" >> sdc_test_tri_injector_wl_$index.txt &
+	timeout $ANOTHER_TIME python injector.py false "/bin/kill -s 37" >> sdc_test_tri_injector_wl_$index.txt &
 	sleep $ANOTHER_TIME
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> sdc_test_tri_injector_wl_$index.txt
 	sleep 40
@@ -138,7 +138,7 @@ mv *.txt $OUTPUT/sdc_wl/
 
 sleep 60
 
-# All Tri /bin/kill -s 38 tests (simulated control flow error)
+# 08 All Tri /bin/kill -s 38 tests (simulated control flow error)
 cp $CONFIG_DIR/all_tri_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > freeze_test_tri_wl_$index.txt &
@@ -146,7 +146,7 @@ for index in `seq 0 $ITARS`; do
 	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
 	sleep 5
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > freeze_test_tri_injector_wl_$index.txt
-	timeout $ANOTHER_TIME python injector.py "/bin/kill -s 38" >> freeze_test_tri_injector_wl_$index.txt &
+	timeout $ANOTHER_TIME python injector.py false "/bin/kill -s 38" >> freeze_test_tri_injector_wl_$index.txt &
 	sleep $ANOTHER_TIME
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> freeze_test_tri_injector_wl_$index.txt
 	sleep 40
@@ -156,7 +156,7 @@ mv *.txt $OUTPUT/freeze_wl/
 
 sleep 60
 
-# All Tri inject_error tests
+# 09 All Tri inject_error tests
 cp $CONFIG_DIR/all_tri_wl.cfg ./config_plumber.cfg
 for index in `seq 0 $ITARS`; do
 	timeout $PLAYER_TIME player baseline.cfg > fault_test_tri_wl_$index.txt &
@@ -164,7 +164,7 @@ for index in `seq 0 $ITARS`; do
 	timeout $BASIC_TIME $PINT_DIR/stage_control/basic $SIM_IP &
 	sleep 5
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm > fault_test_tri_injector_wl_$index.txt
-	timeout $ANOTHER_TIME python injector.py "./inject_error" >> fault_test_tri_injector_wl_$index.txt &
+	timeout $ANOTHER_TIME python injector.py false "./inject_error" >> fault_test_tri_injector_wl_$index.txt &
 	sleep $ANOTHER_TIME
 	ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm >> fault_test_tri_injector_wl_$index.txt
 	sleep 40
